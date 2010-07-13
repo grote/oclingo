5c6335572efb8011057f9635e2ae9ab88ca8523b
rkaminski <rkaminski@3e02f596-3131-0410-8352-e04eeaec37cc>
2010-07-13 22:09:51 +0000

    git-svn-id: https://potassco.svn.sourceforge.net/svnroot/potassco/trunk/gringo@3186 3e02f596-3131-0410-8352-e04eeaec37cc

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 version=3.0.0
 files=$(ls -d {lib{gringo,clasp,lua,luasql,program_opts},lemon,cmake,app,CMakeLists.txt,Makefile,README,INSTALL,CHANGES,COPYING})
-gringo_gen=$(ls build/release/libgringo/src/{converter.cpp,converter_impl.cpp,converter_impl.h,parser.cpp,parser_impl.cpp,parser_impl.h})
-make static target=all
+make static  target=all
+make mingw32 target=all
+gringo_gen=$(ls build/static/libgringo/src/{converter.cpp,converter_impl.cpp,converter_impl.h,parser.cpp,parser_impl.cpp,parser_impl.h})
 rm -rf build/dist
 mkdir -p build/dist
 for x in gringo clingo iclingo; do
@@ -11,10 +12,13 @@ for x in gringo clingo iclingo; do
 	cp ${gringo_gen} build/dist/${x}-${version}-source/libgringo/src/
 	(cd build/dist; tar -czf ${x}-${version}-source.tar.gz ${x}-${version}-source)
 	mkdir -p build/dist/{${x}-${version}-win32,${x}-${version}-x86-linux}
-	#cp build/mingw32/bin/$x.exe CHANGES COPYING build/dist/${x}-${version}-win32
+	cp build/mingw32/bin/$x.exe CHANGES COPYING build/dist/${x}-${version}-win32
 	cp build/static/bin/$x CHANGES COPYING build/dist/${x}-${version}-x86-linux
 	(cd build/dist; tar -czf ${x}-${version}-x86-linux.tar.gz ${x}-${version}-x86-linux)
 	(cd build/dist; zip -r  ${x}-${version}-win32.tar.gz ${x}-${version}-win32)
 	rm -rf build/dist/${x}-${version}-{win32,x86-linux,source}
+	mkdir -p ${x}/${version}
+	scp -r ${x}/${version} rkaminski,potassco@frs.sourceforge.net:/home/frs/project/p/po/potassco/
+	scp CHANGES build/dist/${x}-${version}{-win32.tar.gz,-x86-linux.tar.gz,-source.tar.gz} rkaminski,potassco@frs.sourceforge.net:/home/frs/project/p/po/potassco/${x}/${version}/
 done
 
