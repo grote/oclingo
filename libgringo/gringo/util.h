751a0ada3175956c49a50d39fd7c0809172b23e4
rkaminski <rkaminski@3e02f596-3131-0410-8352-e04eeaec37cc>
2010-12-26 20:15:36 +0000

    - updated copyright info
    git-svn-id: https://potassco.svn.sourceforge.net/svnroot/potassco/trunk/gringo@3883 3e02f596-3131-0410-8352-e04eeaec37cc

--- libgringo/gringo/util.h
+++ libgringo/gringo/util.h
@@ -1,3 +1,14 @@
+// Boost.Range library
+//
+//  Copyright Neil Groves 2009. Use, modification and
+//  distribution is subject to the Boost Software License, Version
+//  1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+//
+// For more information, see http://www.boost.org/libs/range/
+//
+
 #include <boost/iterator/zip_iterator.hpp>
 #include <boost/range/iterator_range.hpp>
 #include <boost/tuple/tuple.hpp>
