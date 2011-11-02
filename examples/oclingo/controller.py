4038336d99389d5920162477a7f029c8a30f0361
Torsten Grote <t@grobox.de>
2011-11-02 19:06:22 +0100

    added testscript and expected outputs (need checking)

--- examples/oclingo/controller.py
+++ examples/oclingo/controller.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #########################################################################
-#   Copyright 2010 Torsten Grote
+#   Copyright 2010-2011 Torsten Grote
 #
 #	This program is free software: you can redistribute it and/or modify
 #	it under the terms of the GNU General Public License as published by
